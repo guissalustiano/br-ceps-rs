6292243391c3cd507b439c528051b1ca1e11de8b
Guilherme Salustiano <guissalustiano@gmail.com>
2025-03-30 01:51:48 -0300

    add generated code

--- src/ceps.rs
+++ src/ceps.rs
@@ -1,7 +1,3 @@
-use crate :: CepRecord ; pub (crate) static CEPS : phf :: Map < & 'static str , CepRecord > =::phf::Map {
-    key: 12913932095322966823,
-    disps: &[
-    ],
-    entries: &[
-    ],
-};
+version https://git-lfs.github.com/spec/v1
+oid sha256:4434e7fab3decdbb878fd7bc8488f22e678aceaedf49b213f3f1a28f65359523
+size 226874047
